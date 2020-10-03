a61c8d26d729f804c43c2bd5d615721a12d1ecd6
pierre <marchand.pierre63@gmail.com>
2020-10-03 13:36:06 +0200

    sauvegarde code parsing response ok

--- telco.py
+++ telco.py
@@ -16,63 +16,50 @@ def config() :
         exit(-1)
 
 def connexion(tx, rx) : 
-    tx.send_command("AT")
-    if -1 == rx.wait_ok(["OK"], 3) :
-        log.warning("AT ERROR")
-    else :
-        log.warning("MODULE READY")
+    log.warning("====CONNECT===")
+    send_command("AT", ["OK"], tx, rx, 3)
+    send_command("AT", ["OK"], tx, rx, 3)
+    send_command("AT", ["OK"], tx, rx, 3)
+    send_command("AT+CPIN?", ["READY"], tx, rx, 3)
+    send_command("AT+CSQ", ["CSQ: \d\d,\d\d"], tx, rx, 3)
+    send_command("AT+COPS?", ["COPS:.*\".*\"", "OK"], tx, rx, 10)
+    send_command("AT+CGNAPN", ["CGNAPN:.*\".*\""], tx, rx, 3)
+    send_command("AT+CNACT", ["COPS:.*\".*\"", "ERROR"], tx, rx, 10, args=["0", "1"])
+    send_command("AT+CNACT?", ["CNACT: 0,1,\".+\""], tx, rx, 10)
 
-    tx.send_command("AT+CPIN?")
-    if -1 == rx.wait_ok(["READY"], 3) :
-        log.warning("SIM ERROR")
-    else : 
-        log.warning("SIM READY")
-
-    tx.send_command("AT+CSQ")
-    resp = rx.wait_ok(["CSQ: \d\d,\d\d"], 3)
-    if -1 == resp:
-        log.warning("CSQ cde Error")
+def send_command(command, expected, tx, rx, tmo_s, args=[], doFlush=True) :
+    tx.send_command(command, args=args)
+    response = rx.wait_ok(expected, tmo_s)
+    if -1 ==  response :
+        # log.warning("CDE {} args : {} ERROR : {}".format(command, args, response))
+        buffer, resp = rx.get_rx_buffer()
+        print("!!! DUMP BUFFER : {}".format(buffer))
+        print("!!! DUMP RESPONSE: {}".format(resp))
     else :
-        resp = resp.split(" ")
-        resp = resp[1].split(",")
-        if int(resp[0]) != 99 :
-            log.debug("NETWORK READY")
-        else :
-            log.warning("network signal error")
+        # log.warning("CDE {} args : {} OK : {}".format(command, args, response))
+        log.warning("OK")
+    if doFlush :
+        rx.flush_rx()
 
-    tx.send_command("AT+CGATT?")
-    if -1 == rx.wait_ok(["CGATT: 1"], 3) :
-        log.warning("network not attached")
-    else :
-        log.debug("NETWORK ATTACHED")
-
-    tx.send_command("AT+COPS?")
-    resp = rx.wait_ok(["COPS:.*\".*\""], 3)
-    if -1 == resp :
-        log.warning("network info error")
-    else :
-        log.debug("network info : {}".format(resp.split("\"")[1]))
-
-    tx.send_command("AT+CGNAPN")
-    resp = rx.wait_ok(["CGNAPN:.*\".*\""], 3)
-    if -1 == resp :
-        log.warning("network info error")
-    else :
-        log.debug("apn info : {}".format(resp.split("\"")[1]))
-
-    tx.send_command("AT+CNACT", ["0", "1"])
-    resp = rx.wait_ok(["ACTIVE", "ERROR"], 3)
-    if -1 == resp :
-        log.warning("network activation error")
-    else :
-        print(resp)
-
-    tx.send_command("AT+CNACT?")
-    resp = rx.wait_ok(["CNACT: 0,1,\".+\""], 3)
-    if -1 == resp :
-        log.warning("network activation error")
-    else :
-        print("IP : ", resp.split("\"")[1])
+def send_msg(tx, rx) :
+    log.warning("====SENDING MSG===")
+    send_command("AT+SHCONF", ["OK"], tx, rx, 3, args=["\"URL\"", "\"http://api.mockpoc.fr\""])
+    send_command("AT+SHCONF", ["OK"], tx, rx, 3, args=["\"BODYLEN\"", "1024"])
+    send_command("AT+SHCONF", ["OK"], tx, rx, 3, args=["\"HEADERLEN\"", "350"])
+    send_command("AT+SHCONN", ["OK"], tx, rx, 3)
+    send_command("AT+SHSTATE?", ["SHSTATE: \d", "OK"], tx, rx, 3)
+    send_command("AT+SHCHEAD", ["OK"], tx, rx, 3)
+    send_command("AT+SHAHEAD", ["OK"], tx, rx, 3, args=["\"Host\"","\"api.mockpoc.fr\""])
+    send_command("AT+SHAHEAD", ["OK"], tx, rx, 3, args=["\"auth-token\"","\"...\""])
+    send_command("AT+SHREQ", ["ERROR", "\+SHREQ: \"GET\",\d+,\d+"], tx, rx, 10, args=["\"/light/list\"","1"])
+    send_command("AT+SHREAD", ["\+SHREAD: \d+", "ERROR"], tx, rx, 3, args=["0","13"], doFlush=False)
+    time.sleep(3)
+    buffer, resp = rx.get_rx_buffer()
+    if "OK" in resp[1] :
+        if "+SHREAD" in resp[3] :
+            print("received : {}".format(resp[4]))
+    send_command("AT+SHDISC", ["OK"], tx, rx, 3)
+    
 if __name__ == "__main__" :
     config()
 
@@ -84,7 +71,7 @@ if __name__ == "__main__" :
     tx.start()
 
     connexion(tx, rx)
-
+    send_msg(tx, rx)
     rx.stop()
     tx.stop()
     rx.join()
